86530a47a55b51c430e011f5f49623e1e342714b
Ubuntu <ubuntu@ip-172-31-7-246.us-east-2.compute.internal>
2019-05-01 14:36:08 +0000

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    
   end
 
   def index
@@ -9,8 +10,35 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    # @recipe.ingredients.build(name:'')
+    # @recipe.ingredients.build(name:'')
+    
+    
+  # <%=f.fields_for :ingredients do |ing|%>
+  # <%=ing.label :name%><br>
+  # <%=ing.text_field :name%><br>
+  # <%=ing.label :quantity%><br>
+  # <%=ing.text_field :quantity%><br>
+  # <%end%>
+    
+    
+    
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+   
+    redirect_to @recipe
   end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, ingredients_attributes: [
+        :id, :name, :quantity
+        ]
+        )
+  end
+  
 end
